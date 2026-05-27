1b8106fdf2f95bad85446f18e00b2c08f18763f1
ViSi93 <vlsivert@ntnu.no>
2026-05-27 11:02:32 +0200

    Added admin authorization for tournament engine

---
+++ backend/makeAdmin.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+import User from "./models/user.js";
+
+await mongoose.connect("mongodb://localhost:27017/restless-developers");
+
+const user = await User.findOne({
+  username: "vilde3",
+});
+
+if (!user) {
+  console.log("User not found");
+  process.exit();
+}
+
+user.role = "admin";
+
+await user.save();
+
+console.log("User is now admin");
+
+process.exit();
